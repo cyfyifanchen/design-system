921b5f3138207c2dc73423c6b86ea1457aaad908
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-08-18 19:30:24 +0800

    1

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,3 +1,8 @@
+import Image from "next/image"
+
+import "styles/global.css"
+import { container } from "styles/header.css"
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +10,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className={container}>
+          <Image src="/react.png" width={25} height={25} alt="React Logo" />
+          <div>NextJS App Router</div>
+        </div>
+        <div>{children}</div>
+      </body>
     </html>
   );
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,55 @@
-import { Button, Header } from "ui";
+import Image from "next/image"
+import {
+  container,
+  image,
+  content,
+  actions,
+  title as titleStyle,
+  description as descriptionStyle,
+} from "styles/card.css";
+import { grid } from "./page.css"
+import { button } from "styles/button.css"
 
+const Card = ({ imageSrc, imageAlt, title, description }) => (
+  <div className={container}>
+    <Image
+      src={imageSrc}
+      alt={imageAlt}
+      width={1024}
+      height={1024}
+      className={image}
+    />
+    <div className={content}>
+      <div className={titleStyle}>{title}</div>
+      <div className={descriptionStyle}>{description}</div>
+      <div className={actions}>
+        <Button>Shop more Jordans</Button>
+      </div>
+    </div>
+  </div>
+)
+
+const Button = ({ children, ...props }) => (
+  <button className={button} {...props}>
+    {children}
+  </button>
+
+)
 export default function Page() {
   return (
-    <>
-      <Header text="Web" />
-      <Button />
-    </>
-  );
+    <div className={grid}>
+      <Card
+        imageSrc="/aj1.jpg"
+        imageAlt="aj1"
+        title="Mollit aliquip ullamco excepteur ut Lorem fugiat esse ullamco non in."
+        description="Culpa amet ipsum dolore duis sint esse mollit irure sint dolore non duis excepteur."
+      />
+      <Card
+        imageSrc="/aj2.jpg"
+        imageAlt="aj2"
+        title="Dolor qui occaecat irure ut fugiat eiusmod minim adipisicing eu quis dolor incididunt."
+        description="Veniam eu quis labore deserunt."
+      />
+    </div>
+  )
 }
